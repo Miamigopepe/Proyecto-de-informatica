0a424751a01c1cb9c066b645657f2d65e16b6816
Miamigopepe <ren.fang.zhang@estudiantat.upc.edu>
2025-05-28 14:00:02 +0200

    Delete Proyecto/version4 directory

--- Proyecto/version4/main.py
+++
@@ -1,7 +0,0 @@
-from interface import AirspaceGUI
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = AirspaceGUI(root)
-    root.mainloop()
--- Proyecto/version4/navSegment.py
+++
@@ -1,5 +0,0 @@
-class NavSegment:
-    def __init__(self, origin, destination, distance):
-        self.origin = origin
-        self.destination = destination
-        self.distance = distance
